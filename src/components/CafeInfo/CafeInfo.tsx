a2a69250fd3f39a1c5851404d08aead5dba8615d
Kit_Kat <matafeii38@gmail.com>
2026-04-14 12:38:27 +0300

    add_step_2

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -3,9 +3,9 @@ import styles from "./CafeInfo.module.css";
 export default function CafeInfo() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>☕ Café Feedback</h1>
+      <h1 className={styles.title}>Sip Happens Café</h1>
       <p className={styles.description}>
-        Please rate your experience at our café. Your feedback helps us improve!
+        Please rate our service by selecting one of the options below.
       </p>
     </div>
   );
